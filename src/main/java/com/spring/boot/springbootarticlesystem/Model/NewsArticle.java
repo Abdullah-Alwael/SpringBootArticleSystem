aebef08fca7d952bb7108b222e9cc27b77df9374
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-23 17:42:38 +0300

    Done testing postman

--- src/main/java/com/spring/boot/springbootarticlesystem/Model/NewsArticle.java
+++ src/main/java/com/spring/boot/springbootarticlesystem/Model/NewsArticle.java
@@ -1,6 +1,5 @@
 package com.spring.boot.springbootarticlesystem.Model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
